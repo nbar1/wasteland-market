881489a953ea6cf5eecf6ccf388677c4136e65f6
Nick Barone <xnickbarone@gmail.com>
2018-09-24 21:11:07 -0400

    Use react-ga library for analytics

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-126411028-1');
+
+function fireTracking() {
+	ReactGA.pageview(window.locaion);
+}
 
 ReactDOM.render(
-	<BrowserRouter>
+	<BrowserRouter onUpdate={fireTracking}>
 		<App />
 	</BrowserRouter>,
 	document.getElementById('root'));
